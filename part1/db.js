39427cc8b8d1b1effcc50fbee6d526707e0505f6
Nick Brown <nictionary10@gmail.com>
2025-06-20 04:03:47 +0000

    [automatic commit] 2025-06-20 04:03:46 UTC 122.148.225.89

--- part1/db.js
+++ part1/db.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
-  host: 'localhost'
-  user: process.env.DB_USER, // "dinesmart_user"
-  password: process.env.DB_PASS, // "DevPass123!"
+  host: 'localhost',
+  user: 'db_user',
+  password: 'pass123',
   database: process.env.DB_NAME, // "dinesmart"
   waitForConnections: true,
   connectionLimit: 10

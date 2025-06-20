a5146a396bb9b6e6336dec37b0816dd4ad1692f2
Nick Brown <nictionary10@gmail.com>
2025-06-20 04:52:41 +0000

    [automatic commit] 2025-06-20 04:52:41 UTC 122.148.225.89

--- part1/app.js
+++ part1/app.js
@@ -123,7 +123,8 @@ app.get('/api/walkrequests/open', async (req, res) => {
             request_time,
             duration_minutes,
             location,
-            (SELECT username FROM sub
+            (SELECT username
+
 
             FROM WalkRequests WHERE status = 'open'
             `);

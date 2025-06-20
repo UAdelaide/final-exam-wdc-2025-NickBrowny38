10580ae80548c2b7004e04c4247e57a5d939afb2
Nick Brown <nictionary10@gmail.com>
2025-06-20 04:29:43 +0000

    [automatic commit] 2025-06-20 04:29:42 UTC 122.148.225.89

--- part1/app.js
+++ part1/app.js
@@ -112,7 +112,7 @@ app.get('/api/dogs', async (req, res) => {
 // Route to return open walk requests
 app.get('/api/walkrequests/open', async (req, res) => {
     try {
-        const [rows] = await db.query('SELECT * FROM WalkRequests ');
+        const [rows] = await db.query("SELECT * FROM WalkRequests WHERE status = 'open'");
         res.json(rows);
     } catch (err) {
         res.status(500).json({ error: 'Failed to fetch dogs'});

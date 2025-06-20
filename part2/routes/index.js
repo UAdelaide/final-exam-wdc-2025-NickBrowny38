cedde2c743cb51671222fbc14b25cdb88795b544
Nick Brown <nictionary10@gmail.com>
2025-06-20 08:59:59 +0000

    [automatic commit] 2025-06-20 08:59:59 UTC 122.148.225.89

--- part2/routes/index.js
+++ part2/routes/index.js
@@ -12,7 +12,7 @@ router.post('/login', async (req, res) => {
   var pass = req.body.password;
 
   try {
-    const
+    const connection = await db.getConnection();
     const [rows] = await db.query(
       "SELECT * FROM Users WHERE username = ? AND password = ?",
       [name, pass]

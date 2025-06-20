c3d3073a08014c0534b790b2d8a6c258d5c31a93
Nick Brown <nictionary10@gmail.com>
2025-06-20 05:32:58 +0000

    [automatic commit] 2025-06-20 05:32:58 UTC ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out ;; no servers could be reached

--- part1/app.js
+++ part1/app.js
@@ -142,7 +142,7 @@ app.get('/api/walkers/summary', async (req, res) => {
             SELECT u.username AS walker_username,
             COUNT(DISTINCT wrate.rating_id) AS num_ratings,
             AVG(wrate.rating) AS avg_ratings,
-            (COUNT(DISTINCT wapp.walker_id) AS num_walks,
+            COUNT(DISTINCT wapp.walker_id) AS num_walks,
             FROM Users u
             LEFT JOIN WalkApplications wapp ON u.user_id = wapp.walker_id AND wapp.status = 'completed'
             LEFT JOIN WalkRatings wrate ON u.user_id = wrate.walker_id

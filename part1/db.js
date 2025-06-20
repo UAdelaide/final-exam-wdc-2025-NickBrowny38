d85f1b8ac02ce42e42838a5686381888332cb890
Nick Brown <nictionary10@gmail.com>
2025-06-20 05:44:46 +0000

    [automatic commit] 2025-06-20 05:44:46 UTC 122.148.225.89

--- part1/db.js
+++ part1/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  database: 'dogwalks_db',
+  database: 'DogWalkService',
   waitForConnections: true,
   connectionLimit: 10
 });

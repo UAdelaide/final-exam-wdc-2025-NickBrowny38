7e80353ce033d3d258c440ef417c6d2fc8db4ccf
Nick Brown <nictionary10@gmail.com>
2025-06-20 04:04:12 +0000

    [automatic commit] 2025-06-20 04:04:12 UTC ;; communications error to 208.67.222.222#53: timed out 122.148.225.89

--- part1/db.js
+++ part1/db.js
@@ -4,7 +4,7 @@ const db = mysql.createPool({
   host: 'localhost',
   user: 'db_user',
   password: 'pass123',
-  database: process.env.DB_NAME, // "dinesmart"
+  database: 'dogwalks_db';
   waitForConnections: true,
   connectionLimit: 10
 });

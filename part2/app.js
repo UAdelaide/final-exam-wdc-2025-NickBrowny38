e30967b21baf6cf2a57a08f99f1a0a02ffd2de1a
Nick Brown <nictionary10@gmail.com>
2025-06-20 12:05:28 +0000

    [automatic commit] 2025-06-20 12:05:28 UTC 122.148.225.89

--- part2/app.js
+++ part2/app.js
@@ -34,6 +34,21 @@ app.use('/', indexRouter);
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
+// Route to return Dogs
+app.get('/api/dogs', async (req, res) => {
+    try {
+        const [rows] = await db.query(`
+            SELECT name AS dog_name,
+            size AS size,
+            (SELECT username FROM Users WHERE user_id = owner_id) AS owner_username
+            FROM Dogs
+        `);
+        res.json(rows);
+    } catch (err) {
+        res.status(500).json({ error: 'Failed to fetch dogs'});
+    }
+});
+
 
 // Export the app instead of listening here
 module.exports = app;

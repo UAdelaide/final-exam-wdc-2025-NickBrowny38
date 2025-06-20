418b85f0a8b90bed9aa317a18d0dc89bbe495f75
Nick Brown <nictionary10@gmail.com>
2025-06-20 12:25:48 +0000

    [automatic commit] 2025-06-20 12:25:48 UTC 122.148.225.89

--- part2/app.js
+++ part2/app.js
@@ -4,6 +4,7 @@ const path = require('path');
 require('dotenv').config();
 const session = require('express-session');
 const app = express();
+const db = require('./db');
 
 //app.use(logger('dev'));
 

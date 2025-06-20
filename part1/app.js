ce8f9c565889d84e4d77d2139024c9184d96c508
Nick Brown <nictionary10@gmail.com>
2025-06-20 05:21:36 +0000

    [automatic commit] 2025-06-20 05:21:36 UTC 122.148.225.89

--- part1/app.js
+++ part1/app.js
@@ -142,11 +142,7 @@ app.get('/api/walkers/summary', async (req, res) => {
             SELECT u.username AS walker_username,
             COUNT(DISTINCT wrate.rating_id) AS num_ratings,
             AVG(wrate.rating AS avg_ratings),
-            COUNT(DISTINCT wapp.walker_id) AS num_walks
-            FROM Users u
-            LEFT JOIN WalkApplications wapp ON u.user_id = wapp.walker_id AND wapp.status = 'completed'
-            LEFT JOIN WalkRatings wrate ON wrate.walk_request_id
-            FROM WalkRequests WHERE status = 'open'
+            COUNT(DISTINCT WalkApplications
 
         `);
         res.json(rows);

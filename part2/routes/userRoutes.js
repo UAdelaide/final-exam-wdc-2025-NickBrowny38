ec906beb4e1d69d33cbf42ce3d3c1ef2e6c509d1
Nick Brown <nictionary10@gmail.com>
2025-06-20 10:42:27 +0000

    [automatic commit] 2025-06-20 10:42:27 UTC 122.148.225.89

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -59,7 +59,7 @@ router.post('/login', async (req, res) => {
 
 router.get('/dogs', async (req, res) => {
   const [rows] = await db.query(`
-    SELECT name FROM Dogs
+    SELECT name FROM Dogs WHERE owner_id = 
   `);
 });
 

5617e292dbecd3cf776b27c346a0c9d895813634
Nick Brown <nictionary10@gmail.com>
2025-06-20 04:42:17 +0000

    [automatic commit] 2025-06-20 04:42:17 UTC ;; communications error to 208.67.222.222#53: timed out 122.148.225.89

--- part1/app.js
+++ part1/app.js
@@ -119,7 +119,7 @@ app.get('/api/walkrequests/open', async (req, res) => {
     try {
         const [rows] = await db.query(`
             SELECT request_id,
-            
+            (SELECT name FROM Dogs WHERE dog_id = dog_id)
             FROM WalkRequests WHERE status = 'open'
             `);
         res.json(rows);
